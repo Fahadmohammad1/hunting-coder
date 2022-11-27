5d78e4f747e4ee6f23f1acf77875a0198907c859
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-11-27 21:35:13 +0600

    some blog added to blog page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
-
 const Navbar = () => {
   return (
     <nav className={styles.mainnav}>
--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,38 @@
 import React from "react";
+import styles from "../styles/blog.module.css";
 
 const Blog = () => {
-  return <div>blog</div>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.blogs}>
+        <h2>Popular Blogs</h2>
+        <div className={styles.blogItem}>
+          <h3>How to Learn Javascript in 2022</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora
+            porro maiores similique aperiam dicta minus delectus dolorem quae
+            sit fugiat!
+          </p>
+        </div>
+        <div className={styles.blogItem}>
+          <h3>How to Learn Javascript in 2022</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora
+            porro maiores similique aperiam dicta minus delectus dolorem quae
+            sit fugiat!
+          </p>
+        </div>
+        <div className={styles.blogItem}>
+          <h3>How to Learn Javascript in 2022</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora
+            porro maiores similique aperiam dicta minus delectus dolorem quae
+            sit fugiat!
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Blog;
